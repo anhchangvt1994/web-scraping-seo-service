eccb96cc6fe2f471aa1c0750250a21486c500284
anhchangvt1994 <anhchangvt1994@gmail.com>
2024-09-16 23:53:46 +0700

    (update): update logic of DetectStatic

--- server/src/utils/DetectStaticExtension.ts
+++ server/src/utils/DetectStaticExtension.ts
@@ -1,25 +1,26 @@
-import { Request } from 'express'
+import { Request } from "express";
 
 const detectStaticExtension = (req: Request) => {
-	const url = req.url
+  const url = req.url;
 
-	if (!url) {
-		return false
-	}
+  if (
+    !url ||
+    req.headers["sec-fetch-dest"] === "document" ||
+    /application\/json/.test(req.headers["accept"] as string)
+  ) {
+    return false;
+  }
 
-	let isStatic = false
-	isStatic =
-		/[A-Za-z0-9-]+\.(vue|ts|js|css|gif|jpg|jpeg|png|ico|bmp|ogg|webp|mp4|webm|mp3|ttf|woff|json|rss|atom|gz|zip|rar|7z|css|js|gzip|exe|svg|pdf|docx)(\?[^\/]*)?$/g.test(
-			url
-		)
+  let isStatic = false;
+  isStatic =
+    /[A-Za-z0-9-]+\.(vue|ts|js|css|gif|jpg|jpeg|png|ico|bmp|ogg|webp|mp4|webm|mp3|ttf|woff|json|rss|atom|gz|zip|rar|7z|css|js|gzip|exe|svg|pdf|docx)(\?[^\/]*)?$/g.test(
+      url
+    ) ||
+    /\/(image|asset|video|audio|mp3|mp4|movie|font|css|js|lib|util|plugin|page|component)+(|s)+\//g.test(
+      url
+    );
 
-	if (!isStatic) {
-		isStatic = /^(?!.*(text\/html|application\/json))/.test(
-			req.headers['accept'] as string
-		)
-	}
+  return isStatic;
+};
 
-	return isStatic
-}
-
-export default detectStaticExtension
+export default detectStaticExtension;
--- server/src/utils/DetectStaticExtension.uws.ts
+++ server/src/utils/DetectStaticExtension.uws.ts
@@ -1,25 +1,26 @@
-import { HttpRequest } from 'uWebSockets.js'
+import { HttpRequest } from "uWebSockets.js";
 
 const detectStaticExtension = (req: HttpRequest) => {
-	const url = req.getUrl()
+  const url = req.getUrl();
 
-	if (!url) {
-		return false
-	}
+  if (
+    !url ||
+    req.getHeader("sec-fetch-dest") === "document" ||
+    /application\/json/.test(req.getHeader("accept") as string)
+  ) {
+    return false;
+  }
 
-	let isStatic = false
-	isStatic =
-		/[A-Za-z0-9-]+\.(vue|ts|js|css|gif|jpg|jpeg|png|ico|bmp|ogg|webp|mp4|webm|mp3|ttf|woff|json|rss|atom|gz|zip|rar|7z|css|js|gzip|exe|svg|pdf|docx)(\?[^\/]*)?$/g.test(
-			url
-		)
+  let isStatic = false;
+  isStatic =
+    /[A-Za-z0-9-]+\.(vue|ts|js|css|gif|jpg|jpeg|png|ico|bmp|ogg|webp|mp4|webm|mp3|ttf|woff|json|rss|atom|gz|zip|rar|7z|css|js|gzip|exe|svg|pdf|docx)(\?[^\/]*)?$/g.test(
+      url
+    ) ||
+    /\/(image|asset|video|audio|mp3|mp4|movie|font|css|js|lib|util|plugin|page|component)+(|s)+\//g.test(
+      url
+    );
 
-	if (!isStatic) {
-		isStatic = /^(?!.*(text\/html|application\/json))/.test(
-			req.getHeader('accept') as string
-		)
-	}
+  return isStatic;
+};
 
-	return isStatic
-}
-
-export default detectStaticExtension
+export default detectStaticExtension;
